5cd3e5e31838665e7223cad6a0ebeac31a224af6
Swarup <swarupkum45@gmail.com>
2023-04-27 02:19:57 +0530

    connected to mongodb and api is adding data and retriving data

--- hello.go
+++ hello.go
@@ -1,85 +1,110 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-    "log"
     "context"
-    
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
 
+    "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type student struct {
-	Name  string `json:"name"`
-	Class string `json:"class"`
+    Name  string `json:"name"`
+    Class string `json:"class"`
 }
 
 var students = []student{}
 
 func main() {
-
-    clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
     client, err := mongo.Connect(context.Background(), clientOptions)
     if err != nil {
         log.Fatal(err)
     }
 
-    dbErr := client.Ping(context.Background(), nil)
-    if dbErr != nil {
-        log.Fatal(dbErr)
+    err = client.Ping(context.Background(), nil)
+    if err != nil {
+        log.Fatal(err)
     }
 
     fmt.Println("Connected to MongoDB!")
 
-    handler:= func(w http.ResponseWriter, r *http.Request) {
+    collection := client.Database("studentDB").Collection("students")
+
+    handler := func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello Swarup!")
     }
 
-    http.HandleFunc("/", handler);
+    http.HandleFunc("/", handler)
 
-	http.HandleFunc("/students", studentsHandler)
-    fmt.Println("Server is listening on port 8080...")
+    http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+        switch r.Method {
+        case "POST":
+            createStudent(w, r, collection)
+        case "GET":
+            getStudents(w, r, collection)
+        default:
+            http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+        }
+    })
 
-	connectionErr := http.ListenAndServe(":8080", nil)
-    if connectionErr != nil {
-        panic(connectionErr)
+    fmt.Println("Server is listening on port 4000...")
+    err = http.ListenAndServe(":4000", nil)
+    if err != nil {
+        panic(err)
     }
 }
 
-func studentsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		createStudent(w, r)
-	case "GET":
-		getStudents(w, r)
-	default:
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-	}
+func createStudent(w http.ResponseWriter, r *http.Request, collection *mongo.Collection) {
+    decoder := json.NewDecoder(r.Body)
+    var newStudent student
+    err := decoder.Decode(&newStudent)
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+        return
+    }
+
+    insertResult, err := collection.InsertOne(context.Background(), newStudent)
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Println("New student added with ID:", insertResult.InsertedID)
+
+    w.WriteHeader(http.StatusCreated)
 }
 
-func createStudent(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var newStudent student
-	err := decoder.Decode(&newStudent)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	students = append(students, newStudent)
-    fmt.Println("New student added: ", newStudent)
-	w.WriteHeader(http.StatusCreated)
-}
+func getStudents(w http.ResponseWriter, r *http.Request, collection *mongo.Collection) {
+    cur, err := collection.Find(context.Background(), bson.D{})
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        return
+    }
+    defer cur.Close(context.Background())
 
-func getStudents(w http.ResponseWriter, r *http.Request) {
-	jsonData, err := json.Marshal(students)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", jsonData)
+    var students []student
+    for cur.Next(context.Background()) {
+        var s student
+        err := cur.Decode(&s)
+        if err != nil {
+            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+            return
+        }
+        students = append(students, s)
+    }
+
+    jsonData, err := json.Marshal(students)
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "%s", jsonData)
 }
